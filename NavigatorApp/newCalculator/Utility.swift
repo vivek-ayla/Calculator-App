b83bd8b43979b057f941890908f5233d6c84055d
Vivek Kumar <vik525634@aylanetworks.com>
2024-07-09 12:45:01 +0530

    Created Utility class to handle common functions

--- NavigatorApp/newCalculator/Utility.swift
+++ NavigatorApp/newCalculator/Utility.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+
+class Utility{
+    static public func generateRandomColor() -> UIColor {
+        let redValue = CGFloat(drand48())
+        let greenValue = CGFloat(drand48())
+        let blueValue = CGFloat(drand48())
+        let randomColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+        return randomColor
+    }
+
+}
