353bc28d48c225997c66b3085693f31c4658e194
Vivek K <vik525634@aylanetworks.com>
2024-07-16 12:46:07 +0530

    organized images

--- NavigatorApp/newCalculator/Controller/ViewController.swift
+++ NavigatorApp/newCalculator/Controller/ViewController.swift
@@ -7,16 +7,25 @@
 
 import UIKit
 
+enum images {
+    static let imageOne = UIImage(named: "calculator")
+    static let imageTwo = UIImage(named: "greenScreen")
+    static let imageThree = UIImage(named: "alert1")
+    static let imageFour = UIImage(named: "alert2")
+    static let imageFive = UIImage(named: "alert3")
+}
+
+
 class ViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
     var tableCell:[TableViewCellModel] = [
-        TableViewCellModel(title: "Calculator",subTitle: "Calculator SubTitle" , image: UIImage(named: "anyImage")),
-        TableViewCellModel(title: "Green Screen", subTitle: "Green SubTitle", image: UIImage(named: "")),
-        TableViewCellModel(title: "Alert", subTitle: "Alert SubTitle", image: UIImage(named: "anyImage")),
-        TableViewCellModel(title: "Alert", subTitle: "Alert SubTitle", image: UIImage(named: "anyImage")),
-        TableViewCellModel(title: "Alert", subTitle: "Alert SubTitle", image: UIImage(named: "anyImage"))
+        TableViewCellModel(title: "Calculator",subTitle: "Calculator SubTitle" , image: images.imageOne),
+        TableViewCellModel(title: "Green Screen", subTitle: "Green SubTitle", image: images.imageTwo),
+        TableViewCellModel(title: "Alert", subTitle: "Alert SubTitle", image: images.imageThree),
+        TableViewCellModel(title: "Alert", subTitle: "Alert SubTitle", image: images.imageFour),
+        TableViewCellModel(title: "Alert", subTitle: "Alert SubTitle", image: images.imageFive)
         ]
     
     var viewControllerID:[String] = ["calculatorVC","greenScreenVC" ]
